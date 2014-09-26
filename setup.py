ed6914fd51d624481742059570feac5a2b90a1c7
Anthony LaTorre <tlatorre@uchicago.edu>
2014-09-26 17:34:15 -0500

    update .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="neutrino",
-      version="0.1",
+      version="0.2",
       description="High Performance Redis Timeseries",
       author="Anthony LaTorre",
+      url="https://github.com/tlatorre-uchicago/neutrino",
+      keywords=["redis", "timeseries"],
       py_modules=['neutrino'],
-      requires=['msgpack-python',
-                'redis',
-                'jinja2'],
+      install_requires=['msgpack_python','redis','jinja2'],
      )
